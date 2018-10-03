89308bd892f61ce9adc62f2a5efc67e28fe7b620
Elyse Vest <evest90@gmail.com>
2018-10-03 11:52:35 +0300

    inital commit

---
+++ twoSum.js
@@ -0,0 +1,5 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// console.log(twoSum([2, 7, 11, 15], 9), ' Should equal [ 0, 1 ]');
+// console.log(twoSum([31, 2, 3, 4, 5, 6, 7, 29], 31), ' Should equal [ 1, 7 ]');
